1bc0bfef7eb9469313f4635391b3ddffbe4b160f
b-venter <52171108+b-venter@users.noreply.github.com>
2021-05-29 21:04:39 +0200

    Update certs_coturn.sh

--- certs_coturn.sh
+++ certs_coturn.sh
@@ -9,11 +9,11 @@ coturndom='turn.matrix.example.com'
 echo 'Checking if newer cert exists...'
 
 active=$(echo "Q" | openssl s_client -connect $coturndom:5349 2>/dev/null | openssl x509 -noout -enddate)
+echo $active
 filecert=$(cat /opt/$coturndom/fullchain.cer | openssl x509 -noout -enddate)
+echo $filecert
 
-#find 'path to possibly new' -newer 'path to existing'
-if [ $active==$filecert ];
-then
+if [ "$active" == "$filecert" ]; then
   echo "None newer. Ending."
 else
   echo "File differs and is thus newer. Restarting coturn..."
