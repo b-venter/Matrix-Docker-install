1de1c94442ba94abcdc3c1d055fb2dec159a9d15
b-venter <52171108+b-venter@users.noreply.github.com>
2021-05-29 17:33:47 +0200

    Update certs_coturn.sh

--- certs_coturn.sh
+++ certs_coturn.sh
@@ -6,17 +6,16 @@
 
 coturndom='turn.matrix.example.com'
 
-echo 'Checking if newer cert exists...'               
-                                       
-#find 'path to possibly new' -newer 'path to existing'                           
-if [ "$(find /root/.acme.sh/$coturndom/fullchain.cer -newer /opt/$coturndom/)" ];
-then                                                                             
-        echo "Newer found...copying across to /opt/$coturndom"        
-        cp "/root/.acme.sh/$coturndom/fullchain.cer" "/opt/$coturndom/"
-else                                                                  
-        echo "None newer. Ending."                             
+echo 'Checking if newer cert exists...'
+
+active=$(echo "Q" | openssl s_client -connect turn.matrix.workshop86.com:5349 2>/dev/null | openssl x509 -noout -enddate)
+filecert=$(cat /opt/turn.matrix.workshop86.com/fullchain.cer | openssl x509 -noout -enddate)
+
+#find 'path to possibly new' -newer 'path to existing'
+if [ $active==$filecert ];
+then
+  echo "None newer. Ending."
+else
+  echo "File differs and is thus newer. Restarting coturn..."
+  reboot
 fi
-
-echo "Q" | openssl s_client -connect turn.matrix.workshop86.com:5349 2>/dev/null | openssl x509 -noout -dates
-
-cat /opt/turn.matrix.workshop86.com/fullchain.cer | openssl x509 -noout -enddate
