a8586938e61f7984beb8bde1e88925fe42399f59
b-venter <52171108+b-venter@users.noreply.github.com>
2021-05-30 13:20:10 +0200

    Update certs_acme.sh
    Added docker restart of TURN server

--- certs_acme.sh
+++ certs_acme.sh
@@ -1,8 +1,12 @@
 #!/bin/sh
 
-# Sample file for "acme" machine. Replace example.com with your domain
-# Schedule this with crontab to run once a week
-# It works in harmony with certs_coturn.sh on "coturn" machine.
+# Sample file for "acme" machine. 
+# 1. Replace example.com with your domain
+# 2. Place in "acme" container, /opt/ and make executable (chmod +x)
+# 2B. Or in rancher at /opt/certs/
+# 3. Schedule this with crontab to run once a week
+#   crontab -e
+#   0 0 * * 0   /opt/certs_acme.sh
 
 coturndom='turn.matrix.example.com'
 
@@ -15,6 +19,9 @@ if [ "$(find /root/.acme.sh/$coturndom/fullchain.cer -newer /opt/$coturndom/)" ]
 then                                                                             
         echo "Newer found...copying across to /opt/$coturndom"        
         cp "/root/.acme.sh/$coturndom/fullchain.cer" "/opt/$coturndom/"
+        echo "Restarting TURN server..."                                                                 
+        curl -X POST http://dockerproxy:2375/containers/coturn/restart                                   
+        echo "Done."
 else                                                                  
         echo "None newer. Ending."                             
 fi
